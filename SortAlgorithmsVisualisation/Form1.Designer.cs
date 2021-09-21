e78cbabd82dc909013a9d65a6d5b4e337887e38a
sito270 <69002597+sito270@users.noreply.github.com>
2021-09-21 12:48:32 +0200

    1

--- SortAlgorithmsVisualisation/Form1.Designer.cs
+++ SortAlgorithmsVisualisation/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace SortAlgorithmsVisualisation
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -29,21 +29,72 @@ namespace SortAlgorithmsVisualisation
         /// </summary>
         private void InitializeComponent()
         {
+            this.ActionPanel = new System.Windows.Forms.Panel();
+            this.MenuPanel = new System.Windows.Forms.Panel();
+            this.BanerPanel = new System.Windows.Forms.Panel();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.BanerPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // ActionPanel
+            // 
+            this.ActionPanel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ActionPanel.Location = new System.Drawing.Point(200, 97);
+            this.ActionPanel.Name = "ActionPanel";
+            this.ActionPanel.Size = new System.Drawing.Size(741, 433);
+            this.ActionPanel.TabIndex = 0;
+            // 
+            // MenuPanel
+            // 
+            this.MenuPanel.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.MenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.MenuPanel.Name = "MenuPanel";
+            this.MenuPanel.Size = new System.Drawing.Size(200, 530);
+            this.MenuPanel.TabIndex = 0;
+            // 
+            // BanerPanel
+            // 
+            this.BanerPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BanerPanel.Controls.Add(this.GenerateButton);
+            this.BanerPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BanerPanel.Location = new System.Drawing.Point(200, 0);
+            this.BanerPanel.Name = "BanerPanel";
+            this.BanerPanel.Size = new System.Drawing.Size(741, 100);
+            this.BanerPanel.TabIndex = 0;
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(0, 0);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(212, 97);
+            this.GenerateButton.TabIndex = 0;
+            this.GenerateButton.Text = "Generate a new array";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(941, 530);
+            this.Controls.Add(this.ActionPanel);
+            this.Controls.Add(this.BanerPanel);
+            this.Controls.Add(this.MenuPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Name = "Form1";
+            this.Name = "MainWindow";
             this.Text = "Sort Algorithms Visualisation";
+            this.BanerPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel ActionPanel;
+        private System.Windows.Forms.Panel MenuPanel;
+        private System.Windows.Forms.Panel BanerPanel;
+        private System.Windows.Forms.Button GenerateButton;
     }
 }
 
--- SortAlgorithmsVisualisation/Form1.cs
+++ SortAlgorithmsVisualisation/Form1.cs
@@ -10,11 +10,36 @@ using System.Windows.Forms;
 
 namespace SortAlgorithmsVisualisation
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        int[] TheArray;
+        Graphics g;
+
+
+        public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+
+            g = ActionPanel.CreateGraphics();
+            int NumEntries = ActionPanel.Width;
+            int MaxVal = ActionPanel.Height;
+            TheArray = new int[NumEntries];
+
+
+            g.FillRectangle(new System.Drawing.SolidBrush(System.Drawing.Color.DarkSlateGray), 0, 0, NumEntries, MaxVal);
+
+            Random rand = new Random();
+
+            for(int i = 0; i < NumEntries; i++) TheArray[i] = rand.Next(0, MaxVal);
+            
+
+            for (int i = 0; i < NumEntries; i++) g.FillRectangle(new System.Drawing.SolidBrush(System.Drawing.Color.LightSeaGreen), i, MaxVal - TheArray[i], 1, MaxVal);
+            
+
         }
     }
 }
